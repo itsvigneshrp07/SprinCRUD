40a5f116f20a3f15b72bc1d51f7ed2993520eecf
Vignesh C <vigne@Vignesh-ST107.sakshath-technologies.com>
2023-01-27 09:26:47 +0530

    First SpringBoot commit

---
+++ src/main/java/com/codedecode/demo/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.codedecode.demo.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+@Table(name = "emp")
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "EMPNAME")
+	private String empName;
+}
